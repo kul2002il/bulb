526bb1150d3c8f3431bd1e5d7030e82476099625
Ilya V. Kulmanakov <ikulmanakov@tomsksoft.com>
2023-07-14 02:39:04 +0700

    Show bulbs and rules

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,49 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Rules from './components/Rules.vue'
+import Bulb from './components/Bulb.vue'
+</script>
+
+<script>
+let data = {
+    bulbs: [
+        {
+            balls: [
+                {color: "blue"},
+                {color: "blue"},
+                {color: "blue"},
+            ],
+        },
+        {
+            balls: [
+                {color: "blue"},
+                {color: "blue"},
+                {color: "blue"},
+            ],
+        },
+        {
+            balls: [
+                {color: "blue"},
+                {color: "blue"},
+                {color: "blue"},
+            ],
+        },
+    ],
+};
+
+export default {
+    data() {return data},
+}
+
 </script>
 
 <template>
-    <HelloWorld/>
+    <h1>Пробирки</h1>
+    <Rules/>
+    <br>
+    <Bulb
+        v-for="bulb in bulbs"
+        :data="bulb"
+    />
 </template>
 
 <style>
---
+++ src/components/Bulb.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="bulb">
+        <div
+            v-for="ball in data.balls"
+            class="ball"
+            :style="'background-color: ' + ball.color"
+        ></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Bulb',
+    props: [
+        'data',
+    ]
+}
+</script>
+
+<style scoped>
+.bulb {
+    display: inline-block;
+    border: 2px solid black;
+    border-top-color: #e3e2e2;
+    padding: 10px;
+    margin: 5px;
+    border-radius: 0 0 25px 25px;
+}
+
+.ball {
+    width: 20px;
+    height: 20px;
+    margin: 2px;
+    border: 2px solid black;
+    border-radius: 10px;
+}
+</style>
