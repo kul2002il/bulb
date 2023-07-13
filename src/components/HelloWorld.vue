20eca9f554991d5f050cbc6bb26454d7d7ddf3d1
Ilya V. Kulmanakov <ikulmanakov@tomsksoft.com>
2023-07-14 01:41:01 +0700

    Init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+    msg: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    Hello world, burn in hell.
+</template>
+
+<style>
+</style>
