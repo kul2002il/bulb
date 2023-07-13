a318316d5d24b85e135ef282cf9f83a0c1f59362
Ilya V. Kulmanakov <ikulmanakov@tomsksoft.com>
2023-07-14 04:13:40 +0700

    Worked prototype

--- src/App.vue
+++ src/App.vue
@@ -4,34 +4,74 @@ import Bulb from './components/Bulb.vue'
 </script>
 
 <script>
+
 let data = {
+    state: {
+        select: null
+    },
     bulbs: [
         {
+            capacity: 4,
             balls: [
+                {color: "blue"},
+                {color: "red"},
+                {color: "blue"},
+                {color: "blue"},
+            ],
+        },
+        {
+            capacity: 4,
+            balls: [
+                {color: "red"},
                 {color: "blue"},
                 {color: "blue"},
                 {color: "blue"},
             ],
         },
         {
+            capacity: 4,
             balls: [
                 {color: "blue"},
                 {color: "blue"},
-                {color: "blue"},
+                {color: "red"},
+                {color: "red"},
             ],
         },
         {
-            balls: [
-                {color: "blue"},
-                {color: "blue"},
-                {color: "blue"},
-            ],
+            capacity: 4,
+            balls: [],
         },
     ],
 };
 
 export default {
     data() {return data},
+    methods: {
+        click(bulb_key) {
+            if (this.state.select === null) {
+                if (this.bulbs[bulb_key].balls.length > 0) {
+                    this.state.select = bulb_key;
+                }
+                return;
+            }
+            if (this.state.select === bulb_key) {
+                this.state.select = null;
+                return;
+            }
+            if (
+                this.bulbs[bulb_key].balls.length === 0
+                || this.bulbs[bulb_key].balls[0].color === this.bulbs[this.state.select].balls[0].color
+            ){
+                if (this.bulbs[bulb_key].balls.length >= this.bulbs[bulb_key].capacity) {
+                    return;
+                }
+                this.bulbs[bulb_key].balls.unshift(
+                    this.bulbs[this.state.select].balls.shift()
+                );
+                this.state.select = null;
+            }
+        }
+    },
 }
 
 </script>
@@ -39,12 +79,19 @@ export default {
 <template>
     <h1>Пробирки</h1>
     <Rules/>
-    <br>
-    <Bulb
-        v-for="bulb in bulbs"
-        :data="bulb"
-    />
+    <div class="shelf">
+        <Bulb
+            v-for="(bulb, key) in bulbs"
+            :data="bulb"
+            :selected="state.select===key"
+            @click="click(key)"
+        />
+    </div>
 </template>
 
 <style>
+.shelf {
+    display: flex;
+    flex-direction: row;
+}
 </style>
--- src/components/Bulb.vue
+++ src/components/Bulb.vue
@@ -1,10 +1,16 @@
 <template>
-    <div class="bulb">
+    <div
+        class="bulb"
+        :class="{
+            selected: selected,
+        }"
+    >
         <div
             v-for="ball in data.balls"
             class="ball"
             :style="'background-color: ' + ball.color"
         ></div>
+        <div class="ball-wigth"></div>
     </div>
 </template>
 
@@ -13,18 +19,26 @@ export default {
     name: 'Bulb',
     props: [
         'data',
+        'selected',
     ]
 }
 </script>
 
 <style scoped>
 .bulb {
-    display: inline-block;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
     border: 2px solid black;
     border-top-color: #e3e2e2;
     padding: 10px;
     margin: 5px;
     border-radius: 0 0 25px 25px;
+}
+
+.bulb.selected {
+    position: relative;
+    top: -10px;
 }
 
 .ball {
@@ -33,5 +47,9 @@ export default {
     margin: 2px;
     border: 2px solid black;
     border-radius: 10px;
+}
+
+.ball-wigth {
+    width: 20px;
 }
 </style>
